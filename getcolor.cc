a5580a432f7d4dfc62dd9c62e16a438d38e909a7
Michael Rowinski <rowinski.michael@gmail.com>
2015-03-31 11:15:48 -0400

    Better Coloring
    Darker background and deeper box colors

--- getcolor.cc
+++ getcolor.cc
@@ -32,7 +32,7 @@ static double CamLightW = 1.8;// 1.27536;
 static double CamLightMin = 0.3;// 0.48193;
 //-----------------------------------
 static const vec3 baseColor(1.0, 1.0, 1.0);
-static const vec3 backColor(0.4,0.4,0.4);
+static const vec3 backColor(0.15,0.15,0.15);
 //-----------------------------------
 
 void lighting(const vec3 &n, const vec3 &color, const vec3 &pos, const vec3 &direction,  vec3 &outV)
@@ -57,7 +57,7 @@ vec3 getColour(const pixelData &pixData, const RenderParams &render_params,
       if(render_params.colourType == 0 || render_params.colourType == 1)
 	{
 	  hitColor = hitColor * pixData.normal;
-	  hitColor = (hitColor + 1.0)/2.0;
+	  hitColor = (hitColor + 1.0)/2.5;
 	  hitColor = hitColor*render_params.brightness;
 	  
 	  //gamma correction
