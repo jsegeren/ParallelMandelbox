1b1ae2502305578267711eaa3b1b9d48f380babc
Joshua Segeren <segerej@live.ca>
2015-04-01 21:07:42 -0400

    Fixed bug in cron job

--- convert_image_job.sh
+++ convert_image_job.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-# Cron wrapper to call the convert images script
-
-# Initialisation
-if [ "$1" == "" ]; then
-exit 1
-fi
-
-interval=$1
+# Cron job which runs every minute (this frequency is specified in the crontab)
+# 1) Create /videos/temp folder
+# 2) Move all bmps from /videos to /videos/temp
+# 3) Convert bmps to jpgs
+# 4) Delete bmps from /videos/temp
+# 5) Move jpgs back to /videos
+# 6) Delete the /videos/temp folder
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export LD_LIBRARY_PATH=/u50/skeeteel/local_installs/usr/local/lib
 BIN_PATH=/u50/skeeteel/local_installs/usr/local/bin
 
-next=$(date +%M | awk -v interval=$interval '{ print int($0 * 60 % interval) }')
-
-if [ $next -gt 0 ]; then
-next=$(echo $next | awk -v interval=$interval '{print interval - $0}')
-fi
-
-
-for i in $(seq $next $interval 59)
-do
-(sleep $i; $BIN_PATH/mogrify -format jpg -quality 80 $DIR/videos/*.bmp; rm $DIR/videos/*.bmp;) &
-done
+(mkdir $DIR/videos/temp; mv $DIR/videos/*.bmp $DIR/videos/temp; $BIN_PATH/mogrify -format jpg -quality 87 $DIR/videos/temp/*.bmp; rm $DIR/videos/temp/*.bmp; mv $DIR/videos/temp/* $DIR/videos; rm -rf $DIR/videos/temp;) &
